1965301285c8b18865ad761a6f3333e2584717b0
barbosafs <sbarbosa.fernando@gmail.com>
2019-06-11 10:09:35 +0200

    Clean up

--- stl_aeplanner/src/stl_aeplanner.cpp
+++ stl_aeplanner/src/stl_aeplanner.cpp
@@ -6,6 +6,7 @@
 #include <numeric>
 
 #include <queue>
+#include <algorithm>
 
 #include <omp.h>
 
@@ -77,19 +78,7 @@ void STLAEPlanner::execute(const stl_aeplanner_msgs::aeplannerGoalConstPtr& goal
   ROS_WARN("Init");
   root_ = initialize(&rtree, current_state);
   ROS_WARN("expandRRT");
-  ROS_WARN_STREAM(root_->gain_ << " " << root_->children_.size());
-  if (root_->gain_ > 0.75 or !root_->children_.size() or
-      root_->score(stl_rtree, ltl_lambda_, ltl_min_distance_, ltl_max_distance_, ltl_min_distance_active_,
-                  ltl_max_distance_active_, ltl_max_search_distance_, params_.bounding_radius, ltl_step_size_,
-                  ltl_routers_, ltl_routers_active_, params_.lambda, ltl_min_altitude_, ltl_max_altitude_,
-                  ltl_min_altitude_active_, ltl_max_altitude_active_) < params_.zero_gain)
-  {
-    expandRRT(ot, &rtree, stl_rtree, current_state);
-  }
-  else
-  {
-    best_node_ = root_->children_[0];
-  }
+  expandRRT(ot, &rtree, stl_rtree, current_state);
 
   ROS_WARN("getCopyOfParent");
   best_branch_root_ = best_node_->getCopyOfParentBranch();
@@ -172,28 +161,25 @@ void STLAEPlanner::initializeKDTreeWithPreviousBestBranch(value_rtree* rtree, st
   {
     rtree->insert(
         std::make_pair(point(current_node->state_[0], current_node->state_[1], current_node->state_[2]), current_node));
-    if (current_node->children_.size())
+    if (!current_node->children_.empty())
       current_node = current_node->children_[0];
-  } while (current_node->children_.size());
+  } while (!current_node->children_.empty());
 }
 
 void STLAEPlanner::reevaluatePotentialInformationGainRecursive(std::shared_ptr<RRTNode> node)
 {
-  ROS_DEBUG_STREAM("Reevaluating!!");
   std::pair<double, double> ret = gainCubature(node->state_);
   node->state_[3] = ret.second;  // Assign yaw angle that maximizes g
   node->gain_ = ret.first;
   for (typename std::vector<std::shared_ptr<RRTNode>>::iterator node_it = node->children_.begin();
        node_it != node->children_.end(); ++node_it)
     reevaluatePotentialInformationGainRecursive(*node_it);
-  ROS_DEBUG_STREAM("Reevaluating done!!");
 }
 
 void STLAEPlanner::expandRRT(std::shared_ptr<octomap::OcTree> ot, value_rtree* rtree,
                              std::shared_ptr<point_rtree> stl_rtree, const Eigen::Vector4d& current_state)
 {
   // Expand an RRT tree and calculate information gain in every node
-  ROS_DEBUG_STREAM("Entering expanding RRT");
   for (int n = 0;
        (n < params_.init_iterations or
         (n < params_.cutoff_iterations and
@@ -204,7 +190,6 @@ void STLAEPlanner::expandRRT(std::shared_ptr<octomap::OcTree> ot, value_rtree* r
        ros::ok();
        ++n)
   {
-    ROS_DEBUG_STREAM("In expand RRT iteration: " << n);
     std::shared_ptr<RRTNode> new_node = std::make_shared<RRTNode>();
     std::shared_ptr<RRTNode> nearest;
     octomap::OcTreeNode* ot_result;
@@ -219,30 +204,15 @@ void STLAEPlanner::expandRRT(std::shared_ptr<octomap::OcTree> ot, value_rtree* r
     {
       Eigen::Vector4d offset = sampleNewPoint();
       new_node->state_ = current_state + offset;
-
       nearest = chooseParent(*rtree, stl_rtree, new_node, params_.extension_range);
-
       new_node->state_ = restrictDistance(nearest->state_, new_node->state_);
-
-      ROS_DEBUG_STREAM("Trying node (" << new_node->state_[0] << ", " << new_node->state_[1] << ", "
-                                       << new_node->state_[2] << ")");
-      ROS_DEBUG_STREAM("    nearest (" << nearest->state_[0] << ", " << nearest->state_[1] << ", " << nearest->state_[2]
-                                       << ")");
       ot_result = ot->search(octomap::point3d(new_node->state_[0], new_node->state_[1], new_node->state_[2]));
       if (ot_result == NULL)
         continue;
-      ROS_DEBUG_STREAM("ot check done!");
-
-      // ROS_DEBUG_STREAM("Inside boundaries?  " << isInsideBoundaries(new_node->state_));
-      // ROS_DEBUG_STREAM("In known space?     " << ot_result);
-      // ROS_DEBUG_STREAM("Collision?          " << collisionLine(
-      //                      nearest->state_, new_node->state_,
-      //                      params_.bounding_radius));
+      
     } while (!isInsideBoundaries(new_node->state_) or !ot_result or
              collisionLine(stl_rtree, nearest->state_, new_node->state_, params_.bounding_radius));
 
-    ROS_DEBUG_STREAM("New node (" << new_node->state_[0] << ", " << new_node->state_[1] << ", " << new_node->state_[2]
-                                  << ")");
     // new_node is now ready to be added to tree
     new_node->parent_ = nearest;
     nearest->children_.push_back(new_node);
@@ -251,16 +221,12 @@ void STLAEPlanner::expandRRT(std::shared_ptr<octomap::OcTree> ot, value_rtree* r
     rewire(*rtree, stl_rtree, new_node, params_.extension_range, params_.bounding_radius, params_.d_overshoot_);
 
     // Calculate potential information gain for new_node
-    ROS_DEBUG_STREAM("Get gain");
     std::pair<double, double> ret = getGain(new_node);
     new_node->state_[3] = ret.second;  // Assign yaw angle that maximizes g
     new_node->gain_ = ret.first;
-    ROS_DEBUG_STREAM("Insert into KDTREE");
     rtree->insert(std::make_pair(point(new_node->state_[0], new_node->state_[1], new_node->state_[2]), new_node));
 
     // Update best node
-
-    ROS_DEBUG_STREAM("Update best node");
     if (!best_node_ or
         new_node->score(stl_rtree, ltl_lambda_, ltl_min_distance_, ltl_max_distance_, ltl_min_distance_active_,
                         ltl_max_distance_active_, ltl_max_search_distance_, params_.bounding_radius, ltl_step_size_,
@@ -271,11 +237,7 @@ void STLAEPlanner::expandRRT(std::shared_ptr<octomap::OcTree> ot, value_rtree* r
                               ltl_step_size_, ltl_routers_, ltl_routers_active_, params_.lambda, ltl_min_altitude_,
                               ltl_max_altitude_, ltl_min_altitude_active_, ltl_max_altitude_active_))
       best_node_ = new_node;
-
-    ROS_DEBUG_STREAM("iteration Done!");
   }
-
-  ROS_DEBUG_STREAM("expandRRT Done!");
 }
 
 Eigen::Vector4d STLAEPlanner::sampleNewPoint()
@@ -298,8 +260,16 @@ std::shared_ptr<RRTNode> STLAEPlanner::chooseParent(const value_rtree& rtree, st
   // Find nearest neighbour
   // TODO: How many neighbours to look for?
   std::vector<value> nearest;
-  rtree.query(boost::geometry::index::nearest(point(node->state_[0], node->state_[1], node->state_[2]), 15),
+  point bbx_min(node->state_[0] - (2 * l), node->state_[1] - (2 * l), node->state_[2] - (2 * l));
+  point bbx_max(node->state_[0] + (2 * l), node->state_[1] + (2 * l), node->state_[2] + (2 * l));
+  box query_box(bbx_min, bbx_max);
+  rtree.query(boost::geometry::index::intersects(query_box), std::back_inserter(nearest));
+
+  if (nearest.empty())
+  {
+    rtree.query(boost::geometry::index::nearest(point(node->state_[0], node->state_[1], node->state_[2]), 15),
               std::back_inserter(nearest));
+  }
 
   // TODO: Check if correct
   std::shared_ptr<RRTNode> best_node;
@@ -406,7 +376,6 @@ std::pair<double, double> STLAEPlanner::getGain(std::shared_ptr<RRTNode> node)
 bool STLAEPlanner::reevaluate(stl_aeplanner_msgs::Reevaluate::Request& req,
                               stl_aeplanner_msgs::Reevaluate::Response& res)
 {
-  ROS_DEBUG_STREAM("Reevaluate start!");
   for (std::vector<geometry_msgs::Point>::iterator it = req.point.begin(); it != req.point.end(); ++it)
   {
     Eigen::Vector4d pos(it->x, it->y, it->z, 0);
@@ -414,230 +383,13 @@ bool STLAEPlanner::reevaluate(stl_aeplanner_msgs::Reevaluate::Request& req,
     res.gain.push_back(gain_response.first);
     res.yaw.push_back(gain_response.second);
   }
-  ROS_DEBUG_STREAM("Reevaluate done!");
 
   return true;
-}
-
-std::vector<octomap::point3d> getChildren(octomap::point3d current, octomap::point3d origin, double resolution)
-{
-  octomap::point3d transformed = current - origin;
-
-  // Source:
-  // https://stackoverflow.com/questions/29557459/round-to-nearest-multiple-of-a-number
-  // transformed.x() = int(((transformed.x() + resolution / 2.0) / resolution)) *
-  // resolution; transformed.y() = int(((transformed.y() + resolution / 2.0) /
-  // resolution)) * resolution; transformed.z() = int(((transformed.z() + resolution
-  // / 2.0) / resolution)) * resolution;
-
-  std::vector<octomap::point3d> children;
-
-  std::vector<double> x_values;
-  if (transformed.x() == 0)
-  {
-    x_values.push_back(current.x() - resolution);
-    x_values.push_back(current.x());
-    x_values.push_back(current.x() + resolution);
-  }
-  else if (transformed.y() == 0 && transformed.z() == 0)
-  {
-    double sign = (transformed.x() > 0) ? 1 : -1;
-
-    x_values.push_back(current.x() + (sign * resolution));
-  }
-  else
-  {
-    double sign = (transformed.x() > 0) ? 1 : -1;
-
-    x_values.push_back(current.x() + (sign * resolution));
-    x_values.push_back(current.x());
-  }
-
-  std::vector<double> y_values;
-  if (transformed.y() == 0)
-  {
-    y_values.push_back(current.y() - resolution);
-    y_values.push_back(current.y());
-    y_values.push_back(current.y() + resolution);
-  }
-  else if (transformed.y() == 0 && transformed.z() == 0)
-  {
-    double sign = (transformed.y() > 0) ? 1 : -1;
-
-    y_values.push_back(current.y() + (sign * resolution));
-  }
-  else
-  {
-    double sign = (transformed.y() > 0) ? 1 : -1;
-
-    y_values.push_back(current.y() + (sign * resolution));
-    y_values.push_back(current.y());
-  }
-
-  std::vector<double> z_values;
-  if (transformed.z() == 0)
-  {
-    z_values.push_back(current.z() - resolution);
-    z_values.push_back(current.z());
-    z_values.push_back(current.z() + resolution);
-  }
-  else if (transformed.x() == 0 && transformed.y() == 0)
-  {
-    double sign = (transformed.z() > 0) ? 1 : -1;
-
-    z_values.push_back(current.z() + (sign * resolution));
-  }
-  else
-  {
-    double sign = (transformed.z() > 0) ? 1 : -1;
-
-    z_values.push_back(current.z() + (sign * resolution));
-    z_values.push_back(current.z());
-  }
-
-  for (double x : x_values)
-  {
-    for (double y : y_values)
-    {
-      for (double z : z_values)
-      {
-        if (x == 0 && y == 0 && z == 0)
-        {
-          // TODO: Maybe some rounding errors?!
-          continue;
-        }
-
-        children.emplace_back(current.x() + x, current.y() + y, current.z() + z);
-      }
-    }
-  }
-
-  return children;
 }
 
 std::pair<double, double> STLAEPlanner::gainCubature(Eigen::Vector4d state)
 {
   std::shared_ptr<octomap::OcTree> ot = ot_;
-
-  // octomap::point3d origin(state[0], state[1], state[2]);
-
-  // std::queue<octomap::point3d> open_set;
-  // open_set.emplace(int(((origin.x() + ot->getResolution() / 2.0) /
-  // ot->getResolution())) *
-  //                      ot->getResolution(),
-  //                  int(((origin.y() + ot->getResolution() / 2.0) /
-  //                  ot->getResolution())) *
-  //                      ot->getResolution(),
-  //                  int(((origin.z() + ot->getResolution() / 2.0) /
-  //                  ot->getResolution())) *
-  //                      ot->getResolution());
-
-  // std::set<std::tuple<double, double, double>> closed_set;
-  // std::set<std::tuple<double, double, double>> unexplored_set;
-
-  // std::vector<int> gain_per_yaw(360, 0);
-
-  // double half_vfov = params_.vfov / 2.0;
-
-  // octomap::point3d current;
-  // while (!open_set.empty())
-  // {
-  //   current = open_set.front();
-  //   open_set.pop();
-  //   if (!closed_set.emplace(current.x(), current.y(), current.z()).second)
-  //   {
-  //     // Have already processed this
-  //     continue;
-  //   }
-
-  //   octomap::OcTreeNode* result = ot->search(current);
-  //   if (result)
-  //   {
-  //     if (result->getLogOdds() > 0)
-  //     {
-  //       // Already seen as Occupied space
-  //       continue;
-  //     }
-  //   }
-  //   else
-  //   {
-  //     if (!unexplored_set.emplace(current.x(), current.y(), current.z()).second)
-  //     {
-  //       // Already processed this
-  //       continue;
-  //     }
-  //     else
-  //     {
-  //       // Is this correct?
-  //       int yaw =
-  //           std::round(std::atan2(origin.y() - current.y(), origin.x() - current.x()) *
-  //                      180.0 / M_PI);
-  //       if (yaw < 0)
-  //       {
-  //         yaw += 360;
-  //       }
-
-  //       gain_per_yaw[yaw]++;
-  //     }
-  //   }
-
-  //   // Get children
-  //   std::vector<octomap::point3d> children =
-  //       getChildren(current, origin, ot->getResolution());
-
-  //   for (octomap::point3d child : children)
-  //   {
-  //     // Check if inside boundaries
-  //     if (!isInsideBoundaries(child))
-  //     {
-  //       // Outside boundaries
-  //       continue;
-  //     }
-
-  //     double distance = (child - origin).norm();
-
-  //     // Check if in sensor fov
-  //     double vertical_angle =
-  //         std::fabs(std::asin((child - origin).z() / distance)) * 180.0 / M_PI;
-  //     if (vertical_angle > half_vfov || distance > params_.r_max)
-  //     {
-  //       // Outside sensor fov or range
-  //       continue;
-  //     }
-
-  //     open_set.push(child);
-  //   }
-  // }
-
-  // // double gain = pow(ot->getResolution(), 3.0) * free_set.size();
-
-  // int half_hfov = params_.hfov / 2;
-
-  // int best_yaw = 0;
-  // int best_yaw_score =
-  //     std::accumulate(gain_per_yaw.begin(), gain_per_yaw.begin() + half_hfov, 0) +
-  //     std::accumulate(gain_per_yaw.rbegin(), gain_per_yaw.rend() - half_hfov,
-  //                     0);  // FIXME: Should the second one be -?
-  // int previous_yaw_score = best_yaw_score;
-
-  // for (int yaw = 1; yaw < 360; ++yaw)
-  // {
-  //   int current_yaw_score =
-  //       previous_yaw_score -
-  //       gain_per_yaw[(yaw - 1 - half_hfov + gain_per_yaw.size()) % gain_per_yaw.size()]
-  //       + gain_per_yaw[(yaw + half_hfov) % gain_per_yaw.size()];
-
-  //   previous_yaw_score = current_yaw_score;
-  //   if (current_yaw_score > best_yaw_score)
-  //   {
-  //     best_yaw_score = current_yaw_score;
-  //     best_yaw = yaw;
-  //   }
-  // }
-
-  // double gain = pow(ot->getResolution(), 3.0) * best_yaw_score;
-
-  // return std::make_pair(gain, best_yaw * M_PI / 180.0);
 
   double gain = 0.0;
 
@@ -800,13 +552,11 @@ bool STLAEPlanner::collisionLine(std::shared_ptr<point_rtree> stl_rtree, Eigen::
 
 void STLAEPlanner::octomapCallback(const octomap_msgs::Octomap& msg)
 {
-  ROS_DEBUG_STREAM("Freeing ot_");
   octomap::AbstractOcTree* aot = octomap_msgs::msgToMap(msg);
   octomap::OcTree* ot = (octomap::OcTree*)aot;
   ot_ = std::make_shared<octomap::OcTree>(*ot);
 
   delete ot;
-  ROS_DEBUG_STREAM("Freeing ot_ done:");
 }
 
 void STLAEPlanner::publishEvaluatedNodesRecursive(std::shared_ptr<RRTNode> node)
@@ -820,7 +570,6 @@ void STLAEPlanner::publishEvaluatedNodesRecursive(std::shared_ptr<RRTNode> node)
     {
       stl_aeplanner_msgs::Node pig_node;
       pig_node.gain = (*node_it)->gain_;
-      // ROS_ERROR_STREAM("GAIN: " << pig_node.gain);
       pig_node.pose.pose.position.x = (*node_it)->state_[0];
       pig_node.pose.pose.position.y = (*node_it)->state_[1];
       pig_node.pose.pose.position.z = (*node_it)->state_[2];
@@ -864,8 +613,6 @@ void STLAEPlanner::agentPoseCallback(const geometry_msgs::PoseStamped& msg)
   if (add_to_ltl_path)
   {
     ltl_path_.poses.push_back(msg);
-
-    ROS_DEBUG("Publish LTL Path");
     ltl_path_.header.frame_id = "map";
     ltl_path_.header.stamp = ros::Time::now();
     ltl_path_pub_.publish(ltl_path_);
@@ -874,7 +621,6 @@ void STLAEPlanner::agentPoseCallback(const geometry_msgs::PoseStamped& msg)
   // Stats
   if (ot_)
   {
-    ROS_DEBUG("Publish LTL Stats");
     stl_aeplanner_msgs::LTLStats ltl_stats;
     ltl_stats.header.stamp = ros::Time::now();
     ltl_stats.ltl_min_distance = (ltl_min_distance_active_) ? ltl_min_distance_ : -1;
@@ -1048,58 +794,6 @@ struct
     return lhs.second < rhs.second;
   }
 } compareByDistance;
-
-// void STLAEPlanner::createLTLSearchDistance()
-// {
-//   if (!ot_)
-//   {
-//     return;
-//   }
-
-//   ltl_search_distances_.clear();
-
-//   double res = ot_->getResolution();
-
-//   for (double x = -ltl_max_search_distance_; x <= 0; x += res)
-//   {
-//     for (double y = -ltl_max_search_distance_; y <= 0; y += res)
-//     {
-//       double distance = std::hypot(x, y);
-
-//       if (distance <= ltl_max_search_distance_)
-//       {
-//         ltl_search_distances_.emplace_back(octomap::point3d(x, y, 0), distance);
-//         ltl_search_distances_.emplace_back(octomap::point3d(x, -y, 0), distance);
-//         ltl_search_distances_.emplace_back(octomap::point3d(-x, y, 0), distance);
-//         ltl_search_distances_.emplace_back(octomap::point3d(-x, -y, 0), distance);
-//       }
-//     }
-//   }
-
-//   std::sort(ltl_search_distances_.begin(), ltl_search_distances_.end(),
-//             compareByDistance);
-// }
-
-// double STLAEPlanner::getDistanceToClosestOccupiedBounded(std::shared_ptr<octomap::OcTree>
-// ot,
-//                                                       Eigen::Vector4d current_state)
-// {
-//   octomap::point3d state(current_state[0], current_state[1], current_state[2]);
-
-//   for (std::pair<octomap::point3d, double> point : ltl_search_distances_)
-//   {
-//     octomap::OcTreeNode* node = ot->search(state + point.first);
-//     if (node)
-//     {
-//       if (ot->isNodeOccupied(node))
-//       {
-//         return point.second;
-//       }
-//     }
-//   }
-
-//   return 10000000;
-// }
 
 void STLAEPlanner::configCallback(stl_aeplanner::STLConfig& config, uint32_t level)
 {
